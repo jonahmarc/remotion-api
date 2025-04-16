33a1bd49419408fa1b1917b86b8435a4a18bbcba
jonahmarc <olympusfoxtrot@gmail.com>
2025-04-16 21:52:03 +0800

    change port

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ app.post('/api/hello', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
